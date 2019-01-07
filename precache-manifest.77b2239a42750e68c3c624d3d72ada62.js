2eb2f2fd2b19a8cac74eb80fa11331f66cb7d2fc
taher elsheikh <taher@gwmail.gwu.edu>
2019-01-06 20:58:59 -0800

    Updates

---
+++ precache-manifest.77b2239a42750e68c3c624d3d72ada62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysideproject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "73e9586637bc3c66a109a02ff0e941cc",
+    "url": "/mysideproject/static/media/appleIcon1.73e95866.svg"
+  },
+  {
+    "revision": "94d0949ba541c1ba916c",
+    "url": "/mysideproject/static/js/runtime~main.94d0949b.js"
+  },
+  {
+    "revision": "aaa600d7c21a2a04e9b2",
+    "url": "/mysideproject/static/js/main.aaa600d7.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/mysideproject/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "aaa600d7c21a2a04e9b2",
+    "url": "/mysideproject/static/css/main.33c67973.chunk.css"
+  },
+  {
+    "revision": "327480ecca43980f5bf5c747205f8697",
+    "url": "/mysideproject/index.html"
+  }
+];
