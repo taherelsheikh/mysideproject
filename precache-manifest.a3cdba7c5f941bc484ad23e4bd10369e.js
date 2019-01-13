81481aa21a70b7d72c6e6f7c1e976f55f55adcd3
taher elsheikh <taher@gwmail.gwu.edu>
2019-01-13 15:56:46 -0800

    Updates

---
+++ precache-manifest.a3cdba7c5f941bc484ad23e4bd10369e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89abb331a271a22af753",
+    "url": "/static/js/main.89abb331.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "89abb331a271a22af753",
+    "url": "/static/css/main.f4cd15a3.chunk.css"
+  },
+  {
+    "revision": "2048e1b730134f69409f46477964708a",
+    "url": "/index.html"
+  }
+];
